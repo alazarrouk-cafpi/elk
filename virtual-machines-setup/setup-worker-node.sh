dec68c48de7a5a4b32488fb4f7139ddde236ae07
alaaeddinezarrouk <alaazarrouk6799@gmail.com>
2024-07-08 17:17:59 +0100

    update setup scripts

--- virtual-machines-setup/setup-worker-node.sh
+++ virtual-machines-setup/setup-worker-node.sh
@@ -6,7 +6,7 @@ sudo apt install curl net-tools wget unzip openjdk-17-jdk
 
 #---Nfs configuration
 sudo apt-get install nfs-common
-
+sudo modprobe nf_conntrack
 echo "net.netfilter.nf_conntrack_max=131072" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 sudo systemctl restart snap.microk8s.daemon-kubelite
